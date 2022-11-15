d0bba7274887a5173927386d82c84cf8970dd823
Laura Hernandez <lvhernandezrincon@gmail.com>
2022-11-15 00:00:37 -0800

    Initial Commit

---
+++ CineJournal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CineJournal
+//
+//  Created by Laura Hernandez on 11/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
